0b8b8a80b699cc2cdcc0368321b664bbca0fe367
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-02-17 16:48:34 +0100

    MAJ code du LanceurDEchec. (Appel à jouer()).

--- src/fr/iutvalence/java/echec/LanceurDEchecs.java
+++ src/fr/iutvalence/java/echec/LanceurDEchecs.java
@@ -19,6 +19,7 @@ public class LanceurDEchecs
 		System.out.println("Création d'une partie d'échec");
 		Echec partieDEchec = new Echec();
 		System.out.println("Démarrage de la partie d'échec");
+		partieDEchec.jouer();
 		System.out.println("Fin de la partie.");
 
 	}

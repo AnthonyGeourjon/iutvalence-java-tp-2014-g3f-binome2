feca6f3de3c0c13357cd6409272f34151a2dcbb0
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-02-17 16:58:28 +0100

    Création des attributs :
    -compteurDeTour qui compte les coups
    -joueurNoire et joueurBlanc sont les joueurs de la partie.

--- src/fr/iutvalence/java/echec/Echec.java
+++ src/fr/iutvalence/java/echec/Echec.java
@@ -1,13 +1,28 @@
 package fr.iutvalence.java.echec;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class Echec. Est une partie d'échec
  */
 public class Echec
 {
 
+	/** Le compteur de tour. */
+	public int compteurDeTour ;
 	
+	/** Le joueur noire. */
+	public Joueur joueurNoire;
 	
+	/** Le joueur blanche. */
+	public Joueur joueurBlanche;
+	
+	/**
+	 * Instancie une nouvel partie d'echec.
+	 */
+	public Echec()
+	{
+		
+	}
 	
 	/**
 	 * Jouer. Simule une partie de jeu d'échec

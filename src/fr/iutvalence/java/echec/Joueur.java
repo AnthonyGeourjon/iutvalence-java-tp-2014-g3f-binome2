ae1c2a430b5ed2cdb3ab367488e9fe881a2a2fce
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-02-17 17:28:16 +0100

    MAJ Javadoc echiquier et joueur

--- src/fr/iutvalence/java/echec/Joueur.java
+++ src/fr/iutvalence/java/echec/Joueur.java
@@ -1,5 +1,9 @@
 package fr.iutvalence.java.echec;
 
+/**
+ * The Class Joueur.
+ *  Cette classe permet d'instancier des joueurs d'échecs
+ */
 public class Joueur
 {
 
--- src/fr/iutvalence/java/echec/echiquier.java
+++ src/fr/iutvalence/java/echec/echiquier.java
@@ -1,5 +1,9 @@
 package fr.iutvalence.java.echec;
 
+/**
+ * The Class echiquier.
+ * Represente le support de jeu
+ */
 public class echiquier
 {
 

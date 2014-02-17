1b61e18e571a29732e18f48aa94d805ffc951441
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-02-17 16:52:28 +0100

    Ecriture du corps de la méthode jouer qui simule la partie.

--- src/fr/iutvalence/java/echec/Echec.java
+++ src/fr/iutvalence/java/echec/Echec.java
@@ -1,5 +1,21 @@
 package fr.iutvalence.java.echec;
+
+/**
+ * The Class Echec. Est une partie d'échec
+ */
 public class Echec
 {
+
+	
+	
+	
+	/**
+	 * Jouer. Simule une partie de jeu d'échec
+	 */
+	public void jouer()
+	{
+		// TODO Auto-generated method stub
+
+	}
 
 }

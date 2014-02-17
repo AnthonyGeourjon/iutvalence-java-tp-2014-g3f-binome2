e6b6788f11b844a498f0155d51a6dd3ab4b309cf
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-02-17 17:27:37 +0100

    MAJ de la méthode jouer
    Le blanc joue 
    Le noire joue
    incrémentation compteur
    
    on recommence

--- src/fr/iutvalence/java/echec/Echec.java
+++ src/fr/iutvalence/java/echec/Echec.java
@@ -8,34 +8,43 @@ public class Echec
 {
 
 	/** Le compteur de tour. */
-	public int compteurDeTour ;
-	
+	public int compteurDeTour;
+
 	/** Le joueur noire. */
 	public final Joueur joueurNoire;
-	
+
 	/** Le joueur blanc. */
 	public final Joueur joueurBlanc;
-	
+
 	/** L'echiquier de la partie. */
 	public echiquier echiquierDeLaPartie;
-	
+
 	/**
 	 * Instancie une nouvel partie d'echec.
 	 */
 	public Echec()
 	{
-		this.compteurDeTour=0;
-		this.joueurBlanc=new Joueur();
-		this.joueurNoire=new Joueur();
-		this.echiquierDeLaPartie=new echiquier();
+		this.compteurDeTour = 0;
+		this.joueurBlanc = new Joueur();
+		this.joueurNoire = new Joueur();
+		this.echiquierDeLaPartie = new echiquier();
 	}
-	
+
 	/**
 	 * Jouer. Simule une partie de jeu d'échec
+	 * Fin represente la condition d'une fin de partie
+	 * On incrémente le compteur de coups
 	 */
 	public void jouer()
 	{
-		// TODO Auto-generated method stub
+		boolean fin = false;
+
+		while (!fin)
+		{
+			joueurBlanc.jouerCoup();
+			joueurNoire.jouerCoup();
+			compteurDeTour++;
+		}
 
 	}
 

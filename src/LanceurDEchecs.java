904a1e7865621ef28a76eb38afc8f7652f09179e
anthony.geourjon <anthony.geourjon@iut-valence.fr>
2014-02-17 16:22:11 +0100

    Création du lanceur de l'application

---
+++ src/LanceurDEchecs.java
@@ -0,0 +1,14 @@
+
+public class LanceurDEchecs
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Création d'une partie d'échec");
+		Echec partieDEchec = new Echec();
+		System.out.println("Démarrage de la partie d'échec");
+		System.out.println("Fin de la partie.");
+
+	}
+
+}

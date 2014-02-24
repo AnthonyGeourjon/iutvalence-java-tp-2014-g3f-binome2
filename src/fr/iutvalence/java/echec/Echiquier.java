50bb7bc62a116deba75a0b62b49028917b757a60
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-02-24 16:27:56 +0100

    Ajout des constantes de création du damier

--- src/fr/iutvalence/java/echec/Echiquier.java
+++ src/fr/iutvalence/java/echec/Echiquier.java
@@ -1,15 +1,14 @@
 package fr.iutvalence.java.echec;
 
 /**
- * Represente le support de jeu, c'est un damier de 8 * 8 cases 
- * alternativement noires et blanches. 
- * Sur ces cases on trouve des pièces ou non. 
+ * Represente le support de jeu, c'est un damier de 8 * 8 cases, alternativement
+ * noires et blanches. Sur ces cases on trouve des pièces ou non.
  */
 public class Echiquier
 {
-	
+
 	/** Nombre de lignes d'un échiquier. */
 	public final int NOMBRE_LIGNE_ECHIQUIER = 8;
-	
+
 	public final int NOMBRE_COLONNE_ECHIQUIER = 8;
 }

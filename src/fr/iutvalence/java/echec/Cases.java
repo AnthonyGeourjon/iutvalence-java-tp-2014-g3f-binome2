45331a6f8121612929d0585f710ec0bb45b6d109
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-02-24 16:44:03 +0100

    Création de la classe position

--- src/fr/iutvalence/java/echec/Cases.java
+++
@@ -1,9 +0,0 @@
-package fr.iutvalence.java.echec;
-
-/**
- * Represente la position d'un pièce d'échec sur un échiquier.
- */
-public class Cases
-{
-	
-}
---
+++ src/fr/iutvalence/java/echec/Position.java
@@ -0,0 +1,15 @@
+package fr.iutvalence.java.echec;
+
+/**
+ * Represente la position d'un pièce d'échec sur un échiquier.
+ */
+public class Position
+{
+	
+	/** Ligne de la postion. */
+	private final int ligne ;
+	
+	/** Colonne de la position. */
+	public final int colonne ;
+	
+}

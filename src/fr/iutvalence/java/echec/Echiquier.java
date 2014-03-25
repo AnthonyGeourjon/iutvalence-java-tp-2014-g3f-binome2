67f465f294623bce434712747548fbad80f61db9
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-03-25 16:09:03 +0100

    Deplacement de code de Partie d'echec dans cette classe. Notamment le tableaux de cases

--- src/fr/iutvalence/java/echec/Echiquier.java
+++ src/fr/iutvalence/java/echec/Echiquier.java
@@ -2,5 +2,45 @@ package fr.iutvalence.java.echec;
 
 public class Echiquier
 {
+	private Case[][] Cases;
 
+	public Echiquier(Joueur joueurBlanc, Joueur joueurNoir)
+	{
+		this.Cases=new Case[8][8];
+		
+		placerPieceSurLEchiquier(joueurBlanc, joueurNoir);
+	}
+
+	private void placerPieceSurLEchiquier(Joueur joueurBlanc, Joueur joueurNoir)
+	{
+		for (int coordonneVerticale = 0; coordonneVerticale < 8; coordonneVerticale++)
+		{
+			for (int coordonneHorizontale = 0; coordonneHorizontale < 8; coordonneHorizontale++)
+				Cases[coordonneVerticale][coordonneHorizontale] = new Case(coordonneVerticale, coordonneHorizontale,null);	
+		}
+		
+		Cases[7][7]=new Case(7, 7, new Piece(joueurBlanc));
+		Cases[0][0]=new Case(0, 0, new Piece(joueurNoir));
+	}
+
+	public String toString()
+	{
+		String affichage = "";
+
+		for (int coordonneVerticale = 0; coordonneVerticale < 8; coordonneVerticale++)
+		{
+			for (int coordonneHorizontale = 0; coordonneHorizontale < 8; coordonneHorizontale++)
+			{
+
+				if (this.Cases[coordonneVerticale][coordonneHorizontale].obtenirPiece() != null)
+					affichage += this.Cases[coordonneVerticale][coordonneHorizontale].obtenirPiece()
+							.ObtenirRepersentationTextuelle();
+				else
+					affichage += ".";
+			}
+			affichage += "\n";
+		}
+
+		return affichage;
+	}
 }

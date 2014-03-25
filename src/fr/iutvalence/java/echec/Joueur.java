42901fe4224e4d1af7eccd5e02cdf49f5d7222dc
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-03-25 16:08:33 +0100

    Adaptation a couleur

--- src/fr/iutvalence/java/echec/Joueur.java
+++ src/fr/iutvalence/java/echec/Joueur.java
@@ -7,43 +7,20 @@ import java.util.Scanner;
  */
 public class Joueur {
 
-	/**
-	 * Demander position au joueur via la console.
-	 * 
-	 * @return la position saisie
-	 */
-	private Position demanderPositionAuJoueur() {
-		// TODO demander à l'utilisateur de saisir les coordonnée de la piece et
-		// verifier si la piece lui appartient
+	private Couleur couleurDuJoueur;
 
-		Scanner sc = new Scanner(System.in);
-		int choixVertical, choixHorizontale;
-
-		System.out.println("Saisissez la coordonné verticale :");
-		choixVertical = sc.nextInt();
-		System.out.println("Saisissez la coordonné horizontale:");
-		choixHorizontale = sc.nextInt();
-
-		return new Position(choixVertical, choixHorizontale);
-
+	
+	public Joueur(Couleur couleurDuJoueur)
+	{
+		this.couleurDuJoueur=couleurDuJoueur;
+	}
+	
+	public Couleur getCouleurDuJoueur()
+	{
+		return couleurDuJoueur;
 	}
 
-	/**
-	 * Demander coordonnee de la piece a jouer.
-	 * 
-	 * @return la position de la piece a jouer.
-	 */
-	public Position demanderCoordonneeDeLaPieceAJouer() {
-		return demanderPositionAuJoueur();
-	}
+	
 
-	/**
-	 * Demander coordonnee de destination de la piece jouee.
-	 * 
-	 * @return une position de destination
-	 */
-	public Position demanderCoordonneeDeDestinationDeLaPieceAJouee() {
-		return demanderPositionAuJoueur();
-	}
-
+	
 }

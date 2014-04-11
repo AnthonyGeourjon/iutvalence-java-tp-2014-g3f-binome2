576c6239dfcc40ad0f87fa090529803ac688fea2
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-04-11 16:53:26 +0200

    Mis a jour de l'interaction avec l'utilisateur

--- src/fr/iutvalence/java/echec/Joueur.java
+++ src/fr/iutvalence/java/echec/Joueur.java
@@ -33,16 +33,20 @@ public class Joueur {
 	
 	public Position demanderPosition()
 	{
-		int choixVertical, choixHorizontal;
-		
+		String reponse = new String();
+			
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Saisissez la coordonnée vertical");
-		choixVertical=sc.nextInt();
-		System.out.println("Saisissez la coordonnée horizontal");
-		choixHorizontal=sc.nextInt();
+		System.out.println("Saisissez les coordonné (ligne, colonne) -> (chiffre, lettre).");
+		reponse=sc.next();
 		
-		return new Position(choixHorizontal, choixVertical);
+		int choixVertical = (int) (reponse.charAt(0)-'0');
+		
+		int choixHorizontal = (int) (reponse.charAt(1)-'a'+1);
+		
+		System.out.println( choixHorizontal + " " +  choixVertical);
+		
+		return new Position(choixHorizontal-1, choixVertical-1);
 		
 		
 	}

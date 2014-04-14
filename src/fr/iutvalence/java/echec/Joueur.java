174d0dbd84a0dca161b976d54908c27b978dd179
anthonygeourjon <anthony.geourjon@iut-valence.fr>
2014-04-14 17:01:24 +0200

    MAJ de la mise en forme de la présentation de la méthode d'interaction avec le joueur

--- src/fr/iutvalence/java/echec/Joueur.java
+++ src/fr/iutvalence/java/echec/Joueur.java
@@ -5,58 +5,53 @@ import java.util.Scanner;
 /**
  * Represente un joueur d'echec.
  */
-public class Joueur {
+public class Joueur
+{
 
 	/**
 	 * Couleur du jouer. Aux echecs noirs ou blanc
 	 */
 	private Couleur couleurDuJoueur;
 
-	
 	/**
 	 * Construit un joueur
-	 * @param couleurDuJoueur couleur du joueur qui sera crée 
+	 * 
+	 * @param couleurDuJoueur
+	 *            couleur du joueur qui sera crée
 	 */
 	public Joueur(Couleur couleurDuJoueur)
 	{
-		this.couleurDuJoueur=couleurDuJoueur;
+		this.couleurDuJoueur = couleurDuJoueur;
 	}
-	
+
 	/**
-	 * Obtenir la couleur du joueur 
-	 * @return la couleur du joueur 
+	 * Obtenir la couleur du joueur
+	 * 
+	 * @return la couleur du joueur
 	 */
 	public Couleur getCouleurDuJoueur()
 	{
 		return this.couleurDuJoueur;
 	}
-	
+
 	/**
 	 * @return une position saisie au clavier par le joueur
 	 */
 	public Position demanderPosition()
 	{
 		String reponse = new String();
-			
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Saisissez les coordonné (ligne, colonne) -> (chiffre, lettre).");
-		reponse=sc.next();
-		
-		int numeroDeLigne = (int) (reponse.charAt(0)-'0');
-		
-		int numeroDeColonne = (int) (reponse.charAt(1)-'a'+1);
-		
-		System.out.println( numeroDeColonne + " " +  numeroDeLigne);
-		
-		return new Position(numeroDeLigne-1, numeroDeColonne-1);
-		
-		
+		reponse = sc.next();
+
+		int numeroDeLigne = (int) (reponse.charAt(0) - '0');
+
+		int numeroDeColonne = (int) (reponse.charAt(1) - 'a' + 1);
+
+		return new Position(numeroDeLigne - 1, numeroDeColonne - 1);
+
 	}
-	
 
-
-	
-
-	
 }

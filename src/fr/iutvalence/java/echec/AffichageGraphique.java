5ec8bac14c59959e72f50bf8f2ce2371ff4a425f
geourjoa <geourjoa@PC-B122-02.iut-valence.fr>
2014-05-19 15:32:37 +0200

    Refaction du code

--- src/fr/iutvalence/java/echec/AffichageGraphique.java
+++ src/fr/iutvalence/java/echec/AffichageGraphique.java
@@ -1,11 +1,17 @@
 package fr.iutvalence.java.echec;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
-public class AffichageGraphique implements Affichage
+public class AffichageGraphique implements Runnable, Affichage
 {
+	private JFrame fenetre;
+	
+	private echiquierGraphique echiquierGraphique;
+
 	public AffichageGraphique()
 	{
-		SwingUtilities.invokeLater(new interfaceGraphique());
+		SwingUtilities.invokeLater(this);
 	}
 	
 	@Override
@@ -24,5 +30,24 @@ public class AffichageGraphique implements Affichage
 	public void afficherTour(int numeroTour, Echiquier echiquier)
 	{
 		// TODO exception à traiter !
+	}
+	
+
+	@Override
+	public void run()
+	{
+		this.fenetre = new JFrame();
+		this.echiquierGraphique= new echiquierGraphique();
+		
+		this.fenetre.add(this.echiquierGraphique);
+		this.fenetre.setVisible(true);
+		
+		this.fenetre.setTitle("Echec");
+		this.fenetre.setSize(400, 400);
+		this.fenetre.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+		// Verouillage des dimensions de la fenêtre
+		this.fenetre.setResizable(false);
+
 	}
 }
---
+++ src/fr/iutvalence/java/echec/LanceurDEchecsGraphique.java
@@ -0,0 +1,26 @@
+package fr.iutvalence.java.echec;
+
+/**
+ * Application servant à jouer une partie d'échec
+ */
+public class LanceurDEchecsGraphique
+{
+
+	/**
+	 * Entrée application
+	 * @param args inutile ici
+	 */
+	
+	public static void main(String[] args)
+	{
+		Joueur joueurBlanc = new JoueurClavier(Couleur.BLANC);
+		Joueur joueurNoir = new JoueurClavier(Couleur.NOIR);	
+		
+		PartieDEchecs partieDEchec = new PartieDEchecs(joueurBlanc, joueurNoir, new AffichageGraphique());
+		
+		partieDEchec.jouerUnePartieDEchec();
+
+
+	}
+
+}
